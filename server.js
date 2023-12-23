908e5276cd1ddc8a78280e2f7666059bd8588b95
tiru-2001 <tirumaleshae709@gmail.com>
2023-12-23 10:20:57 +0530

    modified server

--- server.js
+++ server.js
@@ -6,12 +6,6 @@ import dotenv from "dotenv";
 import connectToDatabase from "./config/connectionFile.js";
 import autrouter from "./routers/authRouter.js";
 
-import path from "path";
-
-import { fileURLToPath } from "url";
-
-const __dirname = path.dirname(fileURLToPath(import.meta.url));
-
 dotenv.config();
 connectToDatabase();
 const app = express();
@@ -20,11 +14,9 @@ const port = process.env.PORT || 7000;
 app.use(cors());
 app.use(express.json());
 app.use("/api/v1", autrouter);
-app.use(express.static(path.join(__dirname, "./recommendation")));
-console.log(path.join(__dirname, "./recommendation/build/index.html"));
-console.log(path.join(__dirname, "./recommendation"));
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "./recommendation/build/index.html"));
+
+app.get("/", (req, res) => {
+  res.send("hi");
 });
 
 app.listen(port, () => {
